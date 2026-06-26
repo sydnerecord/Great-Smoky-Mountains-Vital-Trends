511421a7ce1ed583b9d3b3dc4b9ab0dc71cd33ba
Sydne Record (she/her/hers) <sydnerecord@users.noreply.github.com>
2026-06-26 17:07:53 -0400

    Created climate and veg scripts

---
+++ Main/Vegetation.R
@@ -0,0 +1,10 @@
+############################################################################
+# Vegetation analyses ----
+###########################################################################
+
+# Set working directory
+setwd("G:/Shared drives/GRSM_CESU")
+
+# Read in tree data
+trees <- read.csv("Data/Vegetation/trees.csv", header = TRUE, stringsAsFactors = FALSE)
+
